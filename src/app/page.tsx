7c295a86dead3223b3669a57314dcff95de85f74
ali tarichi <tarichiali@gmail.com>
2024-07-15 17:41:23 +0300

    navbar inital commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import { Banner } from "@/components/Banner";
+import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
-  return <Banner />;
+  return (
+    <>
+      <Banner />
+      <Navbar />
+    </>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,12 @@
+import Image from "next/image";
+import logoImage from "../assets/images/logosaas.png";
+
 export const Navbar = () => {
-  return null;
+  return (
+    <div className="container">
+      <div className="py-4">
+        <Image src={logoImage} alt="Saas logo" className="h-12 w-12" />
+      </div>
+    </div>
+  );
 };

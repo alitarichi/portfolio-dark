e109f6ff51470e5b4c1cfeed1cfafbdd6dc3050e
ali tarichi <tarichiali@gmail.com>
2024-07-15 16:58:49 +0300

    banner is implimented

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,16 @@
 export const Banner = () => {
-  return null;
+  return (
+    <div className="py-3 text-center bg-[linear-gradient(to_right,#FCD6FF,#29D8FF,#FFFD80,#F89ABF,#FCD6FF)]">
+      <div className="container">
+        <p className="font-medium">
+          <span className="hidden sm:inline">
+            Introducing a completely redesigned interface -
+          </span>
+          <a href="#" className="underline underline-offset-4">
+            Explore The Demo
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };

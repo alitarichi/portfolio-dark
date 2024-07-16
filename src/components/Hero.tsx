80cfca88f95498add830c48b0762715fcbd3082a
ali tarichi <tarichiali@gmail.com>
2024-07-16 17:55:25 +0300

    hero inital commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,18 @@
 export const Hero = () => {
-  return null;
+  return (
+    <div className="bg-black text-white">
+      <div className="container">
+        <a href="#">
+          <span>Version 2.0 is here</span>
+          <span>Read More</span>
+        </a>
+        <h1>One Task at a Time</h1>
+        <p>
+          Celebrate the joy of accomplishment with an app designed to track your
+          progress, motivate your efforts, and celebrate your successes.
+        </p>
+        <button>Get for free</button>
+      </div>
+    </div>
+  );
 };

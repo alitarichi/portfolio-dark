1e7d0862288074ba1ca069646b2b0ace86bb33b7
ali tarichi <tarichiali@gmail.com>
2024-07-16 18:14:49 +0300

    hero properties added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,13 @@
 export const Hero = () => {
   return (
-    <div className="bg-black text-white">
+    <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px]">
       <div className="container">
-        <a href="#">
-          <span>Version 2.0 is here</span>
-          <span>Read More</span>
-        </a>
+        <div className="flex items-center justify-center">
+          <a href="#" className="border py-1 px-2 rounded-lg">
+            <span>Version 2.0 is here</span>
+            <span>Read More</span>
+          </a>
+        </div>
         <h1>One Task at a Time</h1>
         <p>
           Celebrate the joy of accomplishment with an app designed to track your

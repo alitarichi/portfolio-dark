665ae206d1f0131ae9a879043f32165da9a09b52
ali tarichi <tarichiali@gmail.com>
2024-07-17 17:46:19 +0300

    logo rail

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -4,6 +4,7 @@ import echoLogo from "../assets/images/echo.png";
 import celestialLogo from "../assets/images/celestial.png";
 import pulseLogo from "../assets/images/pulse.png";
 import apexLogo from "../assets/images/apex.png";
+import Image from "next/image";
 
 const images = [
   { src: acmeLogo, alt: "Acme Logo" },
@@ -15,5 +16,23 @@ const images = [
 ];
 
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="bg-black text-white py-[72px]">
+      <div className="container">
+        <h2 className="text-xl text-center text-white/70">
+          Trusted by the world's most innovative teams
+        </h2>
+        <div
+          className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-20 after:w-20 relative
+         before:bg-red-500 after:bg-red-500 after:right-0 before:left-0 before:top-0 after:top-0"
+        >
+          <div className="flex gap-16">
+            {images.map(({ src, alt }) => (
+              <Image src={src} alt={alt} className="flex-none h-8 w-auto" />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };

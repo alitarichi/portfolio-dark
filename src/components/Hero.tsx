636a010d7d6ee40091e1c0a40eb94fd7830243a1
ali tarichi <tarichiali@gmail.com>
2024-07-17 11:58:47 +0300

    removed rainbow color on text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,8 +2,11 @@ export const Hero = () => {
   return (
     <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px]">
       <div className="container">
-        <div className="flex items-center justify-center">
-          <a href="#" className="border py-1 px-2 rounded-lg">
+        <div className=" flex items-center justify-center">
+          <a
+            href="#"
+            className="inline-flex gap-3 border py-1 px-2 rounded-lg border-white/30"
+          >
             <span>Version 2.0 is here</span>
             <span>Read More</span>
           </a>

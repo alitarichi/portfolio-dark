916421bd702cc1283de45de82b7621c0f255b461
ali tarichi <tarichiali@gmail.com>
2024-07-17 17:53:40 +0300

    logoticker fixed

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -17,14 +17,14 @@ const images = [
 
 export const LogoTicker = () => {
   return (
-    <div className="bg-black text-white py-[72px]">
+    <div className="bg-black text-white py-[72px] sm:py-24">
       <div className="container">
         <h2 className="text-xl text-center text-white/70">
           Trusted by the world's most innovative teams
         </h2>
         <div
-          className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-20 after:w-20 relative
-         before:bg-red-500 after:bg-red-500 after:right-0 before:left-0 before:top-0 after:top-0"
+          className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative
+           after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]"
         >
           <div className="flex gap-16">
             {images.map(({ src, alt }) => (

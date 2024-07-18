4563b0c78f4da530aaa5c8d2e9b149d0603b3d8f
ali tarichi <tarichiali@gmail.com>
2024-07-18 16:28:55 +0300

    features fixed

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -20,21 +20,23 @@ const features = [
 
 export const Features = () => {
   return (
-    <div className="bg-black text-white">
+    <div className="bg-black text-white py-[72px] sm:py-24">
       <div className="container">
-        <h2 className="text-center font-bold text-5xl tracking-tighter">
+        <h2 className="text-center font-bold text-5xl sm:text-6xl tracking-tighter">
           Everything you need
         </h2>
-        <p className="text-center mt-5 text-xl text-white/70">
-          Enjoy Customizable lists, team work tools, and smart tracking all in
-          one place . Set tasks, get reminders and see your progress simply and
-          quickly.
-        </p>
-        <div className="mt-16 flex flex-col gap-4">
+        <div className="max-w-xl mx-auto">
+          <p className="text-center mt-5 text-xl text-white/70">
+            Enjoy Customizable lists, team work tools, and smart tracking all in
+            one place . Set tasks, get reminders and see your progress simply
+            and quickly.
+          </p>
+        </div>
+        <div className="mt-16 flex flex-col sm:flex-row gap-4">
           {features.map(({ title, description }) => (
             <div
               key={title}
-              className="border border-white/30 px-5 py-10 text-center rounded-xl"
+              className="border border-white/30 px-5 py-10 text-center rounded-xl sm:flex-1"
             >
               <div className="inline-flex h-14 w-14 bg-white text-black justify-center items-center rounded-lg">
                 <EcosystemIcon />

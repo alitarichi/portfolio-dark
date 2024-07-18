b6cb03efcd2a4949618fd1c7b6f815d73893043d
ali tarichi <tarichiali@gmail.com>
2024-07-18 17:38:34 +0300

    productshowcase fixed

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -3,14 +3,22 @@ import appScreen from "../assets/images/app-screen.png";
 
 export const ProductShowcase = () => {
   return (
-    <div className="bg-black text-white">
+    <div className="bg-black text-white bg-gradient-to-b from-black to-[#5D2CA8] py-[72px]">
       <div className="container">
-        <h2>Intuitive interface</h2>
-        <p>
-          Celebrate the joy of accomplishment with an app designed to track your
-          progress
-        </p>
-        <Image src={appScreen} alt="product screenshot" />
+        <h2 className="text-center text-5xl sm:text-6xl font-bold tracking-tighter">
+          Intuitive interface
+        </h2>
+        <div className="max-w-xl mx-auto">
+          <p className="text-xl text-center text-white/70 mt-5">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress
+          </p>
+        </div>
+        <Image
+          src={appScreen}
+          alt="product screenshot"
+          className="mt-14 mx-auto"
+        />
       </div>
     </div>
   );

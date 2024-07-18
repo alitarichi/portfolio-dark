2d457b1407d134b7a214dcd12cc358570d857691
ali tarichi <tarichiali@gmail.com>
2024-07-18 16:21:45 +0300

    features added

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,3 +1,5 @@
+import EcosystemIcon from "../assets/icons/ecosystem.svg";
+
 const features = [
   {
     title: "Integration Ecosystem",
@@ -20,8 +22,28 @@ export const Features = () => {
   return (
     <div className="bg-black text-white">
       <div className="container">
-        <h2>Everything you need</h2>
-        <p></p>
+        <h2 className="text-center font-bold text-5xl tracking-tighter">
+          Everything you need
+        </h2>
+        <p className="text-center mt-5 text-xl text-white/70">
+          Enjoy Customizable lists, team work tools, and smart tracking all in
+          one place . Set tasks, get reminders and see your progress simply and
+          quickly.
+        </p>
+        <div className="mt-16 flex flex-col gap-4">
+          {features.map(({ title, description }) => (
+            <div
+              key={title}
+              className="border border-white/30 px-5 py-10 text-center rounded-xl"
+            >
+              <div className="inline-flex h-14 w-14 bg-white text-black justify-center items-center rounded-lg">
+                <EcosystemIcon />
+              </div>
+              <h3 className="mt-6 font-bold">{title}</h3>
+              <p className="mt-2 text-white/70">{description}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );

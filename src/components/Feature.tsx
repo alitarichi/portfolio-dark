b1dce17866c53ffdadc771fd61062ea94c5b3fd5
ali tarichi <tarichiali@gmail.com>
2024-07-22 18:39:43 +0300

    features mouse hover effect added

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef } from "react";
 import EcosystemIcon from "../assets/icons/ecosystem.svg";
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
 
 export const Feature = ({
   title,
@@ -10,13 +11,16 @@ export const Feature = ({
   title: string;
   description: string;
 }) => {
+  const offsetX = useMotionValue(-100);
+  const offsetY = useMotionValue(-100);
+  const maskImage = useMotionTemplate`radial-gradient(100px 100px at ${offsetX}px ${offsetY}px, black , transparent )`;
   const border = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const updateMousePosition = (e: MouseEvent) => {
       if (!border.current) return;
       const borderRect = border.current?.getBoundingClientRect();
-      const offsetX = e.x - borderRect?.x;
-      const offsetY = e.y - borderRect?.y;
+      offsetX.set(e.x - borderRect?.x);
+      offsetY.set(e.y - borderRect?.y);
     };
     window.addEventListener("mousemove", updateMousePosition);
     return () => {
@@ -25,16 +29,14 @@ export const Feature = ({
   }, []);
   return (
     <div className="border border-white/30 px-5 py-10 text-center rounded-xl sm:flex-1 relative">
-      <div
+      <motion.div
         className="absolute inset-0 border-2 border-purple-400 rounded-xl"
         style={{
-          WebkitMaskImage:
-            "radial-gradient(100px 100px at 0px 0px, black , transparent )",
-          maskImage:
-            "radial-gradient(100px 100px at 0px 0px, black , transparent )",
+          WebkitMaskImage: maskImage,
+          maskImage,
         }}
         ref={border}
-      ></div>
+      ></motion.div>
       <div className="inline-flex h-14 w-14 bg-white text-black justify-center items-center rounded-lg">
         <EcosystemIcon />
       </div>

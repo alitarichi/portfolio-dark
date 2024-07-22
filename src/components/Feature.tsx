b93396badb72cf16315309f1d838ecddaf98134f
ali tarichi <tarichiali@gmail.com>
2024-07-22 18:30:37 +0300

    some logic added

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect, useRef } from "react";
 import EcosystemIcon from "../assets/icons/ecosystem.svg";
 
 export const Feature = ({
@@ -7,8 +10,31 @@ export const Feature = ({
   title: string;
   description: string;
 }) => {
+  const border = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const updateMousePosition = (e: MouseEvent) => {
+      if (!border.current) return;
+      const borderRect = border.current?.getBoundingClientRect();
+      const offsetX = e.x - borderRect?.x;
+      const offsetY = e.y - borderRect?.y;
+    };
+    window.addEventListener("mousemove", updateMousePosition);
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
   return (
-    <div className="border border-white/30 px-5 py-10 text-center rounded-xl sm:flex-1">
+    <div className="border border-white/30 px-5 py-10 text-center rounded-xl sm:flex-1 relative">
+      <div
+        className="absolute inset-0 border-2 border-purple-400 rounded-xl"
+        style={{
+          WebkitMaskImage:
+            "radial-gradient(100px 100px at 0px 0px, black , transparent )",
+          maskImage:
+            "radial-gradient(100px 100px at 0px 0px, black , transparent )",
+        }}
+        ref={border}
+      ></div>
       <div className="inline-flex h-14 w-14 bg-white text-black justify-center items-center rounded-lg">
         <EcosystemIcon />
       </div>

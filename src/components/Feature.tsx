1f7875bd92bccfcce01e33f525013bc42cddcb43
ali tarichi <tarichiali@gmail.com>
2024-07-22 18:08:53 +0300

    feature inital commit aka mouse effect in features section

---
+++ src/components/Feature.tsx
@@ -0,0 +1,22 @@
+import EcosystemIcon from "../assets/icons/ecosystem.svg";
+
+export const Feature = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
+  return (
+    <div
+      key={title}
+      className="border border-white/30 px-5 py-10 text-center rounded-xl sm:flex-1"
+    >
+      <div className="inline-flex h-14 w-14 bg-white text-black justify-center items-center rounded-lg">
+        <EcosystemIcon />
+      </div>
+      <h3 className="mt-6 font-bold">{title}</h3>
+      <p className="mt-2 text-white/70">{description}</p>
+    </div>
+  );
+};

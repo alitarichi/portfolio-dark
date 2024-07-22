61959109df2f4b925532fb2cebc17f82ce18dd7b
ali tarichi <tarichiali@gmail.com>
2024-07-22 16:02:50 +0300

    some changes in cta

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,21 +1,28 @@
 export const CallToAction = () => {
   return (
     <div className="bg-black text-white py-[72px] text-center">
-      <h2 className="font-bold text-5xl tracking-tighter">Get istant access</h2>
-      <p className="text-xl text-white/70 mt-5">
-        Celebrate the joy of accomplishment with an app designed to track your
-        progress and motivate your efforts.
-      </p>
-      <form action="" className="mt-10 flex flex-col gap-2.5">
-        <input
-          type="email"
-          placeholder="your@email.com"
-          className="h-12 bg-white/20 rounded-lg px-5 font-medium placeholder:text-[#9CA3AF]"
-        />
-        <button className="bg-white text-black h-12 rounded-lg">
-          Get access
-        </button>
-      </form>
+      <div className="container">
+        <h2 className="font-bold text-5xl tracking-tighter sm:text-6xl">
+          Get istant access
+        </h2>
+        <p className="text-xl text-white/70 mt-5">
+          Celebrate the joy of accomplishment with an app designed to track your
+          progress and motivate your efforts.
+        </p>
+        <form
+          action=""
+          className="mt-10 flex flex-col gap-2.5 max-w-sm mx-auto sm:flex-row"
+        >
+          <input
+            type="email"
+            placeholder="your@email.com"
+            className="h-12 bg-white/20 rounded-lg px-5 font-medium placeholder:text-[#9CA3AF]"
+          />
+          <button className="bg-white text-black h-12 rounded-lg px-5">
+            Get access
+          </button>
+        </form>
+      </div>
     </div>
   );
 };

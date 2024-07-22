017eca1cecfacbd61bf5d22561665f79806208a7
ali tarichi <tarichiali@gmail.com>
2024-07-22 16:25:09 +0300

    footer inital commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Banner } from "@/components/Banner";
 import { CallToAction } from "@/components/CallToAction";
 import { FAQs } from "@/components/FAQs";
 import { Features } from "@/components/Features";
+import { Footer } from "@/components/Footer";
 import { Hero } from "@/components/Hero";
 import { LogoTicker } from "@/components/LogoTicker";
 import { Navbar } from "@/components/Navbar";
@@ -18,6 +19,7 @@ export default function Home() {
       <ProductShowcase />
       <FAQs />
       <CallToAction />
+      <Footer />
     </>
   );
 }

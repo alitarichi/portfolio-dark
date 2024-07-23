7bd04b385a86904ccc965be25900b45d192aee45
ali tarichi <tarichiali@gmail.com>
2024-07-23 13:22:49 +0300

    faq animation added

--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import PlusIcon from "../assets/icons/plus.svg";
 import MinusIcoon from "../assets/icons/minus.svg";
 import clsx from "clsx";
+import { motion, AnimatePresence } from "framer-motion";
 
 const items = [
   {
@@ -45,9 +46,29 @@ const AccordingItem = ({
         <span className="flex-1 text-lg font-bold">{question}</span>
         {isOpen ? <MinusIcoon /> : <PlusIcon />}
       </div>
-      <div className={clsx("mt-4", { hidden: !isOpen, "": isOpen === true })}>
-        {answer}
-      </div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{
+              opacity: 0,
+              height: 0,
+              marginTop: 0,
+            }}
+            animate={{
+              opacity: 1,
+              height: "auto",
+              marginTop: "16px",
+            }}
+            exit={{
+              opacity: 0,
+              height: 0,
+              marginTop: 0,
+            }}
+          >
+            {answer}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
